53f79d168fa90d97d718f1ad78c9382a6e007726
Danil Zhaborktiskyi <104435454+danzhv@users.noreply.github.com>
2023-06-19 19:26:03 +0300

    added sample function

---
+++ sampleFunction.py
@@ -0,0 +1,20 @@
+#new posts
+async def sender_bH(event):
+    for i, source in enumerate(FROM):
+        if event.chat_id == source:
+            destination = TO[i]
+            await BotzHubUser.send_message(
+                destination,
+                event.message
+            )
+            break
+
+#existing posts
+for message in messages:
+        if message.date.timestamp() >= date:
+            if message.message:
+                client.send_message(target_channel_username, message.message)
+                time.sleep(60)
+            if message.media:
+                client.send_file(target_channel_username, message.media, caption=message.text)
+                time.sleep(60)
